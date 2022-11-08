676d7332980f22253f275232645dfbdd12db65c6
sharozx16 <sharoz16@gmail.com>
2022-11-09 00:39:54 +0500

    deployment script changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+ 
+  const Pokemon = await hre.ethers.getContractFactory("Pokemon");
+  const pokemon = await Pokemon.deploy("https://gateway.pinata.cloud/ipfs/QmdWW9pB6oe3Q61BPAjuEa4aAVUNH8MDwB3FnPRahENpFM",
+  "Pokemon", "PKM");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await pokemon.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    ` Deployed with address: ${pokemon.address}`
   );
 }
 
